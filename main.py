78e0525215dd4428fe5be136b9b6f9c76c791364
vish94 <visheshahuja@gmail.com>
2015-12-28 18:46:38 +0000

    Updated main

--- main.py
+++ main.py
@@ -4,4 +4,6 @@ print "Enter your name?"
 name = raw_input("> ")
 player.set_name(name)
 
+print "Welcome "+player.get_name()+"! Ready to face this Python adventure? Fight with the dragon till you survive!"
 player.status()
+print "Write 'help' to see all the valid keys"
